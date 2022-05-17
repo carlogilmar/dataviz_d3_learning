fc875a4177f9edde847fd1ed1155e0ef41e44221
Carlo Gilmar <carlogilmar12@gmail.com>
2022-05-17 11:03:05 -0500

    Adding D3

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,1 +1,9 @@
 console.log("Hey, I'm the JS script in your template dude!");
+
+async function drawLineChart() {
+  console.log("Leyendo perro!")
+  const dataset = await d3.json("./../data/data.json")
+  console.log(dataset)
+}
+
+drawLineChart();
